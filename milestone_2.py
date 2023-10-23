ecfb02be6c792eaeea53cf55e4eb7bbc13a6d669
lou-i0 <lothen123@googlemail.com>
2023-10-23 20:06:46 +0100

    chosen random word and showed the result

--- milestone_2.py
+++ milestone_2.py
@@ -1,4 +1,8 @@
 
+# %% 
+# Import relevant Libaries needed for code
+import random   # used for random numbers and selecting random things etc.
+
 # %%  
 # Make a list of favourite fruits call the variable 'word_list'
 word_list  = ['Blueberry', 'Lemon', 'Banana', 'Satsuma', 'Apple']
@@ -6,3 +10,10 @@ word_list  = ['Blueberry', 'Lemon', 'Banana', 'Satsuma', 'Apple']
 # %%
 # Print out word list 
 print(word_list)
+
+# %% 
+# Pick a word from the word list and put into the 'word' variable
+word = random.choice(word_list)
+
+# print out the word chosen
+print(word)
