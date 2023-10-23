87b18903a5e616c2965eef2456b328dea0852116
lou-i0 <lothen123@googlemail.com>
2023-10-23 20:29:29 +0100

    created function to guess a letter

--- milestone_2.py
+++ milestone_2.py
@@ -1,10 +1,10 @@
 
 # %% 
-# Import relevant Libaries needed for code
+# Import relevant Libraries needed for code
 import random   # used for random numbers and selecting random things etc.
 
 # %%  
-# Make a list of favourite fruits call the variable 'word_list'
+# Make list of favourite fruits and call variable 'word_list'
 word_list  = ['Blueberry', 'Lemon', 'Banana', 'Satsuma', 'Apple']
 
 # %%
@@ -12,20 +12,26 @@ word_list  = ['Blueberry', 'Lemon', 'Banana', 'Satsuma', 'Apple']
 print(word_list)
 
 # %% 
-# Pick a word from the word list and put into the 'word' variable
+# Pick a word from word list and put into 'word' variable
 word = random.choice(word_list)
 
 # print out the word chosen
 print(word)
 
 # %%
-# Ask the user for a single letter 
-guess = input('Please guess a alphabetical letter: ')
+#  create function to ask user for single letter 
+def guess_letter():
+    guess = input('Please guess a alphabetical letter: ')
 
-if len(guess) == 1 and guess.isalpha() == True:
-    print(f'Thank you, you have entered {guess}. Good guess!')
-else:
-    print(f'Oops! That is not a valid input. {guess} is not a single alphabetical letter')
+    if len(guess) == 1 and guess.isalpha() == True:
+        print(f'Thank you, you have entered {guess}. Good guess!')
+    else:
+        print(f'Oops! That is not a valid input. {guess} is not a single alphabetical letter')
+    return guess
+
+
+# %%
+guess_1 = guess_letter()
 
 
 # %%
