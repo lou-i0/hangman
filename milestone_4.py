6de4e57de7c3853f0c9b16e3e8f03d3195e38a44
lou-i0 <lothen123@googlemail.com>
2023-10-24 15:44:26 +0100

    fixed code for a class game

--- milestone_4.py
+++ milestone_4.py
@@ -24,29 +24,23 @@ class Hangman:
         self.word_guessed = ['_' for i in self.word]
 
         # The UNIQUE letter count in the word that have not been guessed yet
-        self.num_letters = len(set(self.word)) + 1
+        self.num_letters = len(set(self.word))
 
         self.num_lives = num_lives
         self.word_list = word_list
         self.list_of_guesses = []
-
-    def check_guess(self, guess):
-        # ensure guess letter is lowercase
-        guess = guess.lower()
-        if guess in self.word:
-            print(f'Good guess! {guess} is in the word.')
-
-            for letter in self.word:
-                if letter == guess:
-                    self.word_guessed[self.word.find(letter)] = letter
-                    print(test_game.word_guessed)
-            self.num_letters -= 1
-        else:
-            print(f'Sorry, {guess} is not in the word. Try again.')
-            self.num_lives -= 1
-            print(f'ou have, {self.num_lives} left.')
+        
 
     def ask_for_input(self):
+        ''' 
+        This method asks the user for an input, validates the input given
+        , then calls the check_guess method to se if the guess is in the selected word.
+
+        Additionally , there are no parameters but has a infinite while loop to check and await feedback before
+        going down a particular route. It also provides feedback to the user of the guesses made, how many lives left if used (GAME OVER!)
+        
+        The Loop ends when either all lives have been used, or all letters have been found (GAME COMPLETE).
+        '''
         while True:
             # Ask user to guess a letter and assign to 'guess' variable
             guess = input('Please enter a single alphabetical letter: ')
@@ -64,6 +58,40 @@ class Hangman:
                     print('You have run out of lives. GAME OVER!')
                     break
                 
+                # Code to check if all letters for word have been guessed
+                if ''.join(self.word_guessed) == self.word:
+                    print(f'You have found all the letters for {test_game.word}, Congratulations! GAME COMPLETE')
+                    break
+                else:
+                    print(f'You still have {self.num_letters} letters to find.')
+
+
+    def check_guess(self, guess):
+        '''
+         The check_guess method takes in the guess entered by the user as a parameter. Then, 
+         checks if the guess is in the selected word. If so, provides feedback saying this and where the letter is placed in the word and asks 
+         to enter another letter to guess.If not, a life is lost and shows this to the user, before asking to try again.
+        '''
+        # ensure guess letter is lowercase
+        guess = guess.lower()
+        if guess in self.word:
+            print(f'Good guess! {guess} is in the word.')
+
+            # find guess and replace in right positon in 'word_guessed'
+            for i,  letter_in_word in enumerate(self.word):
+                if letter_in_word == guess:
+                    #self.word_guessed[self.word.find(letter)] = letter
+                    self.word_guessed[i] = letter_in_word
+                    
+            self.num_letters -= 1
+            print(test_game.word_guessed)
+
+        else:
+            print(f'Sorry, {guess} is not in the word. Try again.')
+            self.num_lives -= 1
+            print(f'You have, {self.num_lives} left.')
+
+
 
 
 
